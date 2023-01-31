aec3d7d878baf2a1a95608f033ac39cfa5bf432a
Harsh Srivastava <hr765259@dal.ca>
2023-01-31 17:17:27 -0400

    Fix
    Added a dummy component for Seller Registration comp

---
+++ Bid_For_Good/src/components/seller-registration/index.vue
@@ -0,0 +1,35 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+    <h1>Welcome to Seller Registration</h1>
+  </template>
+  <script lang="ts">
+  import { defineComponent } from 'vue';
+  export default defineComponent({
+    components: {
+    },
+    props: {
+      headerText: {
+        type: String,
+        default: 'header'
+      },
+      list: {
+        type: Array,
+        default: () => {
+          return []
+        }
+      },
+      options: {
+        type: Object,
+        default: () => {
+          return null
+        }
+      }
+    },
+    setup(props) {
+      console.log("Selleer Component")
+    }
+  })
+  </script>
+  <style>
+  </style>
+  
