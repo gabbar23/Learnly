cf56861ee814e3464a4c9c8409a033926e2786f1
Harsh Srivastava <hr765259@dal.ca>
2023-02-15 09:45:37 -0400

    Fix
    Updated url as per change in comps name.

--- FrontEnd/src/components/component.ts
+++ FrontEnd/src/components/component.ts
@@ -4,4 +4,4 @@ export { default as HomePage } from "../components/homepage.vue";
 export { default as Login } from "../components/login.vue";
 export { default as Navigation } from "../components/navigation.vue";
 export { default as BuyerRegistration } from "../components/buyer-registration.vue";
-export { default as BuyerDashboard } from "../components/buyer-dashboard.vue";
+export { default as AdminDashboard } from "../components/admin-dashboard.vue";
