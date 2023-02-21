89d32268c601afe4bc81b8744ef95de3c1219179
Amanjot Singh <amanjots>
2023-02-20 23:11:50 -0400

    sequelize models added

--- server/dev/app.ts
+++ server/dev/app.ts
@@ -3,8 +3,19 @@ import bodyParser from "body-parser";
 
 import { registerRoutes } from "./routes/registerRoutes";
 import { fetch } from "./routes/formRoutes";
-import { sellerModel } from "./models/sellerModel";
-import { StatesCity } from "./models/citiesState";
+// import { sellerModel } from "./models/sellerModel";
+// import { StatesCity } from "./models/citiesState";
+import { UserDetail } from "./models/userDetailModel";
+import { City } from "./models/cityModel";
+
+import { Bidding } from "./models/biddingModel";
+import { Country } from "./models/countryModel";
+import { Report } from "./models/reportsModel";
+import { State } from "./models/stateModel";
+import { LoginDetail } from "./models/loginDetailModel";
+import { Item } from "./models/itemModel";
+
+import { sequelize } from "./util/database";
 
 const app = express();
 
@@ -26,24 +37,54 @@ app.use((_, res, next) => {
 app.use("/api/v1/register", registerRoutes);
 app.use("/api/fetch", fetch);
 
+// UserDetail.hasOne(City);
+UserDetail.belongsTo(City, { foreignKey: "city_id" });
+UserDetail.belongsTo(State, { foreignKey: "state_id" });
+UserDetail.belongsTo(Country, { foreignKey: "country_id" });
+
+// Report belongsTo User
+
+UserDetail.hasMany(Report, { foreignKey: "user_id" });
+Report.belongsTo(UserDetail, { foreignKey: "user_id" });
+
+UserDetail.hasOne(LoginDetail, { foreignKey: "user_id" });
+
+LoginDetail.belongsTo(UserDetail, { foreignKey: "user_id" });
+
+State.hasMany(City, { foreignKey: "state_id" });
+
+// City belongsTo State
+City.belongsTo(State, { foreignKey: "state_id" });
+
+State.hasMany(City, { foreignKey: "state_id" });
+
+// City belongsTo State
+City.belongsTo(State, { foreignKey: "state_id" });
+
+Country.hasMany(State, { foreignKey: "country_id" });
+
+// State belongsTo Country
+State.belongsTo(Country, { foreignKey: "country_id" });
+
+UserDetail.hasMany(Item, { foreignKey: "user_id" });
+
+// Item belongsTo User
+Item.belongsTo(UserDetail, { foreignKey: "user_id" });
+
+// User hasMany Biddings
+UserDetail.hasMany(Bidding, { foreignKey: "user_id" });
+
+// Bidding belongsTo User
+Bidding.belongsTo(UserDetail, { foreignKey: "user_id" });
+
 //syncing models
-sellerModel
+sequelize
   .sync()
   .then((_) => {
-    console.log("Seller Details Loaded");
+    console.log("Models Loaded");
   })
   .catch((error: Error) => {
     console.log(error);
   });
-
-  StatesCity
-  .sync()
-  .then((_) => {
-    console.log("Region Loaded");
-  })
-  .catch((error: Error) => {
-    console.log(error);
-  });
-
 
 app.listen(3000);
---
+++ server/dev/models/userDetailModel.ts
@@ -0,0 +1,50 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from "../util/database";
+
+const UserDetailModel = sequelize.define("UserDetail", {
+  userId: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  firstName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  middleName: {
+    type: DataTypes.STRING,
+  },
+  lastName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  dateOfBirth: {
+    type: DataTypes.DATEONLY,
+    allowNull: false,
+  },
+  gender: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  isVerified: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+  },
+  phone: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  streetAddress: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+  },
+  updatedAt: {
+    type: DataTypes.DATE,
+  },
+});
+
+export const UserDetail = UserDetailModel;
