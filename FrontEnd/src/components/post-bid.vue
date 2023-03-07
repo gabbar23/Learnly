782f4e963d68f0f5b78c4358ac03fd7eee6709f3
Harsh Srivastava <hr765259@dal.ca>
2023-03-07 18:25:06 -0400

    Fix
    Added UI for Post Bid details comp

---
+++ FrontEnd/src/components/post-bid.vue
@@ -0,0 +1,183 @@
+<!-- eslint-disable vue/require-v-for-key -->
+<template>
+  <FormKit type="form" @submit="sellerRegister" enctype="multipart/form-data">
+    <section class="container parent_sect">
+      <FormKit
+        type="text"
+        label="Name Of Offering"
+        v-model="userDetails.nameOfOffering"
+      />
+
+      <FormKit
+        type="number"
+        label="Estimated Value"
+        v-model="userDetails.estimatedValue"
+      />
+
+      <FormKit
+        type="select"
+        label="State"
+        placeholder="Select a State"
+        :options="states"
+        v-model="userDetails.province"
+        @change="triggerChange(userDetails.province)"
+      >
+      </FormKit>
+
+      <FormKit
+        type="select"
+        label="City"
+        placeholder="Select City"
+        :options="cities"
+        v-model="userDetails.city"
+      />
+
+      <FormKit
+        type="text"
+        label="Postal Zip Code"
+        help="format: a1b-c2d | a1bc2d | a1b c2d"
+        v-model="userDetails.postalCode"
+      />
+
+      <FormKit
+        name="idproof"
+        type="file"
+        label="Photo of Items"
+        accept=".jpg,.jpeg,.png"
+        help="Only .pdf,.jpg,.jpeg,.png files allowed"
+        v-model="userDetails.photoId"
+      />
+
+      <FormKit
+        type="textarea"
+        label="Description"
+        v-model="userDetails.description"
+      />
+    </section>
+  </FormKit>
+</template>
+<script lang="ts" setup>
+import { validation } from "@/constants";
+import type {
+  IGetStateDetails,
+  IGetUserDetails,
+  ISelectResponse,
+} from "@/interfaces/seller-registration";
+import router from "@/router";
+import AuthService from "@/services/AuthService";
+import type { register } from "@formkit/core";
+import { defineComponent, onMounted, reactive, ref, watch } from "vue";
+const states = ref<ISelectResponse[]>([]);
+const cities = ref<ISelectResponse[]>([]);
+let userDetails = reactive<IGetUserDetails>({
+  fname: "",
+  lname: "",
+  email: "",
+  phone: "",
+  nameOfOffering: "",
+  estimatedValue: null,
+  photoId: "",
+  description: "",
+  termsCondition: false,
+  address: "",
+  password: "test123",
+  city: "",
+  province: "",
+  postalCode: "",
+  dateOfbirth: new Date(),
+  age: null,
+});
+let isUserAlreadyRegistered = ref<boolean>(false);
+onMounted(async () => {
+  try {
+    await AuthService.getUploadImage();
+    let response = await AuthService.getStates();
+    states.value = [];
+    for (let i = 0; i < response.data.length; i++) {
+      states.value.push({
+        label: response.data[i].province,
+        value: response.data[i].province,
+      });
+    }
+    console.warn(states.value);
+  } catch (e) {
+    console.error("Error in fetching states", e);
+  }
+});
+const login = () => {
+  router.push("/login");
+};
+const buyerPage = () => {
+  router.push("/buyer-details");
+};
+
+const sellerRegister = async (data: any) => {
+  userDetails.age = 20;
+  console.log(data);
+  const body = new FormData();
+  // Finally, we append the actual File object(s)
+  data.idproof.forEach((fileItem: any) => {
+    console.warn(fileItem);
+    body.append("image", fileItem.file);
+  });
+
+  for (const value of body.values()) {
+    console.error(value);
+  }
+
+  for (var key of body.entries()) {
+    console.log(key[0] + ", " + key[1]);
+  }
+
+  userDetails.photoDetail = body;
+  console.warn(body);
+  console.warn(userDetails.photoDetail);
+  const headerConfig = {
+    headers: {
+      "content-type": "multipart/form-data",
+    },
+  };
+  await AuthService.uploadImage(body, headerConfig);
+
+  // const response = await AuthService.register(userDetails);
+};
+
+const checkUserExists = async (email: string) => {
+  console.warn("User Exists", email);
+  try {
+    await AuthService.checkUserExist(email)
+      .then((res) => {
+        console.warn(res);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+    console.log("TEst");
+  } catch {
+    console.error("Error in checking user existence");
+  }
+};
+const triggerChange = async (val: string) => {
+  console.warn(val);
+  cities.value = [];
+  try {
+    let response = await AuthService.getCities();
+    console.log(response);
+    for (let i = 0; i < response.data.length; i++) {
+      cities.value.push({
+        label: response.data[i].cities,
+        value: response.data[i].cities,
+      });
+    }
+    console.warn(cities.value);
+  } catch (e) {
+    console.error("Error in pulling cities");
+  }
+};
+</script>
+<style>
+.parent_sect {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+}
+</style>
