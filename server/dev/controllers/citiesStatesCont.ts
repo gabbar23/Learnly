ad8aa04fe75cc93342d8033e32ad64f6ba3677bf
Amanjot Singh <am854663@dal.ca>
2023-03-10 20:15:06 -0400

    Email push Notification

--- server/dev/controllers/citiesStatesCont.ts
+++ server/dev/controllers/citiesStatesCont.ts
@@ -1,7 +1,6 @@
 import { Request, Response } from "express";
 import { cityProvince } from "../models/citiesState";
 import { sequelize } from "../util/database";
-// import { sequelize } from "../util/database";
 
 console.log("req recevied for fetching city");
 
@@ -28,8 +27,7 @@ const fetchStates = async (req: Request, res: Response) => {
 const fetchCity = async (req: Request, res: Response) => {
   try {
     //retrievel from db
-    console.log("lololol")
-    console.log(req.body)
+    console.log(req.body);
     console.log(req.body.province_name);
     const cities = await cityProvince.findAll({
       where: {
@@ -39,7 +37,7 @@ const fetchCity = async (req: Request, res: Response) => {
       // distinct: true,
       order: [["city", "ASC"]],
     });
-    console.log(cities)
+    console.log(cities);
     res.status(200).json(cities);
   } catch (error) {
     console.error(error);
