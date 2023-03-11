bf24003a9fa70e50a5848b50e9cecbbbea53cc6c
Jill Pujara <jl797275@dal.ca>
2023-03-11 18:24:44 -0400

    Changing the Bid API

--- server/dev/controllers/fetchBidDetails.ts
+++ server/dev/controllers/fetchBidDetails.ts
@@ -1,15 +1,23 @@
 import { Request, Response } from "express";
 import { Bidding } from "../models/biddingModel";
 
-const fetchBidDetails = async (_req: Request, res: Response) => {
+//Fecth the bid details
+const fetchBidDetails = async (req: Request, res: Response) => {
     try {
       console.log("fetch live bid details");
-      const details = await Bidding.findAll({
-        where: {
-            bidType:'live'
-        }
-      });
-      res.status(200).json(details);
+      const bid_type = req.body.bidType;
+      var details ;
+      if(bid_type != null){
+       details = await Bidding.findAll({
+          where: {
+              bidType:bid_type
+          }
+        });
+      }
+      else{
+         details = await Bidding.findAll({});
+      }
+    res.status(200).json(details);
     } catch (error) {
       console.error(error);
       res.status(500).json({ message: "Server error" });
