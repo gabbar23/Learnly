f00c311ab4e7a7d3e4cc4c1f27ec479d9c46bbb6
Kush Shaileshkumar Sutaria <ks428142@dal.ca>
2023-03-12 13:13:58 -0300

    feat(signup): modified the color pallete for better visuals of the form

--- FrontEnd/src/components/registration/seller-registration.vue
+++ FrontEnd/src/components/registration/seller-registration.vue
@@ -12,12 +12,13 @@
           type="text" 
           label="First Name" 
           v-model="userDetails.firstName" 
+          style="color: black;"
           />
 
-      <FormKit type="text" label="Last Name" v-model="userDetails.lastName" />
+      <FormKit type="text" label="Last Name" v-model="userDetails.lastName" style="color: black;" />
 
-      <FormKit type="number" label="Phone Number" v-model="userDetails.phone" />
-      <FormKit type="date" label="Date of birth"/>     
+      <FormKit type="number" label="Phone Number" v-model="userDetails.phone"  style="color: black;"/>
+      <FormKit type="date" label="Date of birth"  style="color: black;"/>     
       <!-- <FormKit
         type="text"
         label="Name Of Offering"
@@ -30,7 +31,7 @@
         v-model="userDetails.estimatedValue"
       /> -->
 
-          <FormKit type="text" label="Address" v-model="userDetails.address" />
+          <FormKit type="text" label="Address" v-model="userDetails.address"  style="color: black;"/>
 
       <FormKit
         type="select"
@@ -39,6 +40,7 @@
         :options="states"
         v-model="userDetails.provinceName"
         @change="triggerChange(userDetails.provinceName)"
+        
       >
       </FormKit>
 
@@ -55,6 +57,7 @@
             label="Postal Zip Code"
             help="format: a1b-c2d | a1bc2d | a1b c2d"
             v-model="userDetails.postalCode"
+            style="color: black;"
           />
 
           <FormKit
@@ -79,15 +82,18 @@
             label="Email"
             v-model="userDetails.email"
             @blur="checkUserExists(userDetails.email)"
+            style="color: black;"
           />
           <div class="error" v-if="isUserAlreadyRegistered">
             User Already Registered.
           </div>
           <br />
 
-          <FormKit type="password" label="Password" v-model="userDetails.password" />
+          <FormKit type="password" label="Password" v-model="userDetails.password" 
+          style="color: black;"/>
 
-          <FormKit type="password" label="Confirm Password" />
+          <FormKit type="password" label="Confirm Password" 
+          style="color: black;"/>
 
           <FormKit
             type="checkbox"
@@ -167,7 +173,7 @@ let userDetails = reactive<IGetUserDetails>({
   cityName: "",
   provinceName: "",
   photoDetail:undefined,
-  postalCode: "a1b-c2d",
+  postalCode: "",
   dateOfBirth: new Date(),
   age: null,
   isBuyer:false,
@@ -301,132 +307,117 @@ const triggerChange = async (val: string) => {
   font-family: Verdana, sans-serif;
 }
 
-
-div#app {
+#app, #app2 {
   width: 100%;
   height: 100%;
   background-color: #eadfdf;
 }
-div#app2{
-  width: 100%;
-  height: 100%;
-justify-content: center;
-background-color: #eadfdf;
-display: flex;
-}
 
-div#app div#login2 {
+#app2 {
+  display: flex;
+  justify-content: center;
   align-items: center;
   background-color: #eadfdf;
-  display:flex;
-  justify-content:center;
-  width:auto;
-  height:auto;
 }
 
-div#app2 div#form {
+#app #login2, #app #login2 #form2 {
+  width: auto;
+  height: auto;
+  background-color: #eadfdf;
+}
+
+#app #login2 {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+}
+
+ #app #login2 #form2 {
+  width:1000px;
+  padding: 35px;
+  border-radius: 5px;
+  box-shadow: 0px 0px 30px 0px #666;
+  color: black;
+  background-color: #eadfdf;
+}
+
+#app2 #form {
+  width:260px;
+  padding: 35px;
+  border-radius: 5px;
+  box-shadow: 0px 0px 30px 0px #666;
+  color: black;
+  justify-content: flex-end;
   background-color: #34495e;
-  border-radius: 5px;
-  box-shadow: 0px 0px 30px 0px #666;
-  color: #eadfdf;
-  justify-content:flex-end;
-  width: 260px;
-  padding: 35px;
 }
 
-div#app2  div#form label,
-div#app2  div#form input {
-  outline: none;
-  width: 100%;
-}
-div#app2 div#form label {
-  color: #eadfdf;
+#app2 #form label {
+  color: black;
   font-size: 0.8em;
 }
 
-div#app div#login2 div#form2 {
-  background-color: teal;
-  border-radius: 5px;
-  box-shadow: 0px 0px 30px 0px #666;
-  color: #eadfdf;
-  width:900px;
-  padding: 35px;
-}
-
-div#app div#login2 div#form2 label{
+#app #login2 #form2 label, #app #login2 #form2 input {
   outline: none;
   width: 100%;
+  color: #000;
 }
 
-div#app div#login2 div#form2 label {
-  color: #eadfdf;
+#app #login2 #form2 label {
+  color: black;
   font-size: 0.8em;
 }
 
-div#app div#login2 div#form2 input {
+#app #login2 #form2 input {
   background-color: transparent;
   border: none;
-  color:#eadfdf;
+  color: transparent;
   font-size: 1em;
   margin-bottom: 15px;
 }
-[data-type="checkbox"] .formkit-input ~ .formkit-decorator, [data-type="radio"] .formkit-input ~ .formkit-decorator  {
-  color: var(--fk-color-primary);
-}
-#input_15 > ul > li:nth-child(1) > label > div > span > span > svg > polygon{
-  color:#eadfdf;
-}
-#input_15 > ul > li:nth-child(2) > label > div > span > span > svg > polygon{
-  color: #eadfdf;
-}
 
-
-div#app div#login2 div#form2 form ::placeholder,
-div#app div#login2 div#form2 select  {
-  color: #eadfdf;
+#app #login2 #form2 form::placeholder, #app #login2 #form2 select {
+  color: black;
   opacity: 1;
 }
 
-div#app div#login2 div#form2 button {
-  background-color: #eadfdf;
+#app #login2 #form2 button,#app2 #form button{
+  background-color: whitesmoke;
   color: #000;
   cursor: pointer;
   border: none;
-  padding: 10px;
+  padding: 15px;
   transition: background-color 0.2s ease-in-out;
+  margin-left: 25px;
   width: 75%;
 }
 
-div#app div#login2 div#form2 button:hover {
+#app #login2 #form2 button:hover,#app2 #form button:hover {
   color: #ecf0f1;
   background-color: #381e1e;
 }
 
 @media screen and (max-width: 600px) {
-  div#app div#login2 {
+  #app #login2 {
     align-items: unset;
-    background-color: unset;
+    background-color: #eadfdf;
     display: unset;
     justify-content: unset;
   }
 
-
-  div#app div#login2 div#form2 {
+  #app #login2 #form2 {
     border-radius: unset;
     box-shadow: unset;
     width: 100%;
+    color: black;
   }
 
-  div#app div#login2 div#form2 form {
+  #app #login2 #form2 form {
     margin: 0 auto;
     max-width: min-content;
     width: 100%;
   }
-  div#app div#login2 div#form2 {
-    color: #eadfdf;
-    
-  }
-  
+
 .parent_sect {
   display: grid;
   grid-template-columns: 
