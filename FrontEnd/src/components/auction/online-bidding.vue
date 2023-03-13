29e3a51737c8a3b45bae07aed5eb99d07f98d9b9
Harsh Srivastava <hr765259@dal.ca>
2023-03-13 12:29:11 -0300

    Fix
    Added loader comp
    Added timer on online bidding

--- FrontEnd/src/components/auction/online-bidding.vue
+++ FrontEnd/src/components/auction/online-bidding.vue
@@ -1,37 +1,40 @@
 <!-- eslint-disable @typescript-eslint/no-unused-vars -->
 <template>
   <div class="main-section w-50 mx-auto m-2">
-    <div class="w-100 m-1">
-      <Carousel :autoplay="2000" :wrap-around="true">
-        <Slide v-for="slide in 4" :key="slide">
-          <div class="carousel__item">{{ slide }}</div>
-        </Slide>
-        <template #addons>
-          <Navigation />
-          <Pagination />
-        </template>
-      </Carousel>
-    </div>
-    <div>
-      <div class="row">
-        <div class="details">
-          <div>Description</div>
-          <div>Closing At: 40$</div>
-          <div>Current Max: 40$</div>
-          <div class="d-flex">
-            <div class="mr-4">Make Bid</div>
-            <FormKit
-              type="form"
-              submit-label="Make Bid"
-              :actions="false"
-              @submit="makeBid"
-            >
-              <FormKit type="text" />
-              <button class="btn btn-danger ml-5" :disabled="isBidMade">
-                Submit Bid
-              </button>
-              <div v-if = "isBidMade">{{ timeLeft }} sec</div>
-            </FormKit>
+    <loader v-if="isLoading"></loader>
+    <div v-else>
+      <div class="w-100 m-1">
+        <Carousel :autoplay="2000" :wrap-around="true">
+          <Slide v-for="slide in 4" :key="slide">
+            <div class="carousel__item">{{ slide }}</div>
+          </Slide>
+          <template #addons>
+            <Navigation />
+            <Pagination />
+          </template>
+        </Carousel>
+      </div>
+      <div>
+        <div class="row">
+          <div class="details">
+            <div>Description</div>
+            <div>Closing At: 40$</div>
+            <div>Current Max: 40$</div>
+            <div class="d-flex">
+              <div class="mr-4">Make Bid</div>
+              <FormKit
+                type="form"
+                submit-label="Make Bid"
+                :actions="false"
+                @submit="makeBid"
+              >
+                <FormKit type="text" />
+                <button class="btn btn-danger ml-5" :disabled="isBidMade">
+                  Submit Bid{{ minVal }}
+                </button>
+                <div v-if="isBidMade">{{ timeLeft }} sec</div>
+              </FormKit>
+            </div>
           </div>
         </div>
       </div>
@@ -44,10 +47,14 @@ import { ref, watch } from "vue";
 import "vue3-carousel/dist/carousel.css";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { Carousel, Slide, Pagination, Navigation } from "vue3-carousel";
+import Loader from "../loader.vue";
 
 const isBidMade = ref<boolean>(false);
 const timeLeft = ref(10); // 60 seconds
 let timer: any;
+const minVal = ref<Number>(5);
+const minValidation = ref<any>({ min: 5 });
+const isLoading = ref<boolean>(false);
 
 const makeBid = () => {
   isBidMade.value = true;
