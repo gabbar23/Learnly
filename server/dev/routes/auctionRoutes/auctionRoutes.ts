5711670c62db8cd9260b472a60441345034d6233
Darshil <dr371813@dal.ca>
2023-03-13 12:20:41 -0300

    small fix pipeline

--- server/dev/routes/auctionRoutes/auctionRoutes.ts
+++ server/dev/routes/auctionRoutes/auctionRoutes.ts
@@ -1,11 +1,11 @@
 import express from "express";
-import auction from "../../controllers/liveAuctionController/auctionHandler";
+import auctionHandler from "../../controllers/liveAuctionController/auctionHandler";
 // import multer from 'multer';
 
 
 const router = express.Router();
 
-router.post("/auctionDetails",auction.getAuctionDetails);
-router.post("/auctionEndTime",auction.endTime);
+router.post("/auctionDetails",auctionHandler.getAuctionDetails);
+router.post("/auctionEndTime",auctionHandler.endTime);
 
 export const auctionRoutes = router;
