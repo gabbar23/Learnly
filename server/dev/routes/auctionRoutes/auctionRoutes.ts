6209a11dd5e5cfea18b4cfa28faf9858fc6c2712
Darshil <dr371813@dal.ca>
2023-03-13 12:29:12 -0300

    folder name changed to fix pipeline

--- server/dev/routes/auctionRoutes/auctionRoutes.ts
+++ server/dev/routes/auctionRoutes/auctionRoutes.ts
@@ -1,11 +1,11 @@
 import express from "express";
-import auctionHandler from "../../controllers/liveAuctionController/auctionHandler";
+import auction from "../../controllers/liveAuctionController/auctionHandler";
 // import multer from 'multer';
 
 
 const router = express.Router();
 
-router.post("/auctionDetails",auctionHandler.getAuctionDetails);
-router.post("/auctionEndTime",auctionHandler.endTime);
+router.post("/auctionDetails",auction.getAuctionDetails);
+router.post("/auctionEndTime",auction.endTime);
 
 export const auctionRoutes = router;
