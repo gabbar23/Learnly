5d9331cbae8a4cbd05f098caa16f8c1c279557f5
Harsh Srivastava <hr765259@dal.ca>
2023-03-16 01:08:56 -0300

    Fix
    Finalized Blind Bid FE

--- FrontEnd/src/components/auction/blind-auction.vue
+++ FrontEnd/src/components/auction/blind-auction.vue
@@ -1,55 +1,110 @@
 <!-- eslint-disable @typescript-eslint/no-unused-vars -->
 <template>
   <div class="main-section w-50 mx-auto m-2">
-    <div>
+    <loader v-if="isLoading"></loader>
+    <template v-else>
       <h3>Blind Auction</h3>
       <Carousel>
-        <Slide v-for="slide in 10" :key="slide">
-          <div class="carousel__item">{{ slide }}</div>
-        </Slide>
+        <template v-if="sellItemDetail.imageDetails.length > 0">
+          <Slide
+            v-for="(item, index) in sellItemDetail.imageDetails"
+            :key="index"
+          >
+            <img
+              :src="item.imgUrl"
+              class="carousel__item item_size"
+              @click="bindClick(item)"
+            />
+          </Slide>
+        </template>
+        <template v-else>
+          <Slide v-for="slide in 5" :key="slide">
+            <div class="carousel__item" @click="bindClick(slide)">
+              {{ slide }}
+            </div>
+          </Slide>
+        </template>
         <template #addons>
           <Navigation />
           <Pagination />
         </template>
       </Carousel>
-    </div>
-    <div>
       <div>
-        <div class="details">
-          <p></p>
-          <div class="d-flex">
-            <div class="mr-4">Make Bid</div>
-            <FormKit type="text" />
-            <button class="btn btn-danger ml-5" :disabled="isItemSold">
-              Submit Bid
-            </button>
-            <p>Bid Already Made.</p>
+        <div>
+          <div class="details">
+            <div class="row p-3">
+              <div class="col-2">Name:</div>
+              <div class="col-10">{{ sellItemDetail.itemName }}</div>
+              <div class="w-100"></div>
+              <div class="col-2">Description:</div>
+              <div class="col-10">{{ sellItemDetail.itemDes }}</div>
+            </div>
+            <div class="d-flex">
+              <div class="mr-4">Make Bid</div>
+              <FormKit type="text" />
+              <button
+                class="btn btn-danger ml-5"
+                @click="makePayment"
+                :disabled="sellItemDetail.isSold"
+              >
+                Submit Bid
+              </button>
+              <p>Bid Already Made.</p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </template>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { ref, onMounted } from "vue";
+import { ref, onMounted, reactive } from "vue";
 import "vue3-carousel/dist/carousel.css";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { Carousel, Slide, Pagination, Navigation } from "vue3-carousel";
 import auctionService from "@/services/auctionService";
 import { useRoute } from "vue-router";
+import type { IGetAuctionItemDetails } from "@/interfaces/auction";
+import { Loader } from "../component";
 
 const route = useRoute();
-const isItemSold = ref<boolean>(true);
+const isLoading = ref<boolean>(false);
+let sellItemDetail = reactive<IGetAuctionItemDetails>({
+  imageDetails: [],
+  createdAt: "",
+  isSold: false,
+  itemDes: "",
+  itemId: 0,
+  itemName: "",
+  startPrice: 0,
+  updatedAt: "",
+  user_id: 0,
+});
 
 onMounted(async () => {
-  // const { itemId } = route.query;
-  const itemId = 1;
-  // try{//userId, itemId and Auction Id
-  await auctionService.getAuctionDetails(itemId);
-  await auctionService.getItemDetails(itemId);
-  // }
+  const { itemId } = route.query;
+  try {
+    isLoading.value = true;
+    const response = await auctionService.getItemDetails(11);
+    sellItemDetail = response.data;
+    await auctionService.getAuctionDetails(11);
+  } catch (e) {
+    console.log(e);
+  } finally {
+    isLoading.value = false;
+  }
 });
+
+const makePayment = () => {
+  const details = localStorage.getItem("userDetails");
+  const { userId } = JSON.parse(details);
+  // console.log();
+};
+
+const bindClick = (args: any) => {
+  console.log("Hello");
+};
 </script>
 
 <style>
@@ -77,5 +132,9 @@ onMounted(async () => {
 .carousel__next {
   box-sizing: content-box;
   border: 5px solid white;
+}
+
+.item_size {
+  height: 300px;
 }
 </style>
