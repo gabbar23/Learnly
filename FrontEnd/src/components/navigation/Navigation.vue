edcb35023f298a77cf150d52888e1029e3942c60
Harsh Srivastava <hr765259@dal.ca>
2023-03-26 15:41:11 -0300

    Fix
    Added logout icon

--- FrontEnd/src/components/navigation/Navigation.vue
+++ FrontEnd/src/components/navigation/Navigation.vue
@@ -5,11 +5,28 @@
     </div>
     <div class="col-sm-4 col-xs-4">
       <!-- <a class="nav-link ml-auto" href="/">Home </a> -->
+      <div class="col-sm-4 col-xs-4">
+        <button class="btn btn-outline-dark" @click="logout">
+          <font-awesome-icon icon="sign-out-alt" />
+        </button>
+      </div>
     </div>
   </nav>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import router from "@/router";
+import { computed } from "vue";
+
+const hideLogOutIcon = computed((router) => {
+  console.warn(router.currentRoute.value);
+  return false;
+});
+
+const logout = () => {
+  console.log("LogOUt");
+};
+</script>
 <style>
 .background {
   background-color: aquamarine;
