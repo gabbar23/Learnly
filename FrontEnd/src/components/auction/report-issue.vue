30180713c3e464c730a88867de47958f7e509bd4
Harsh Srivastava <hr765259@dal.ca>
2023-03-30 23:08:23 -0300

    Fix
    Fixed FE build failure issues

--- FrontEnd/src/components/auction/report-issue.vue
+++ FrontEnd/src/components/auction/report-issue.vue
@@ -3,17 +3,16 @@
     <h1>Report issue</h1>
   </section>
   <div class="text-center">
-    <FormKit
-        type="form"
-      @submit="issueSubmit"
-      >
+    <FormKit type="form" @submit="issueSubmit">
       <FormKit
         type="select"
         label="Issue"
         placeholder="Select the type of issue"
-        :options="[{label:'Return',value:'return'}, 
-        {label:'Refund',value:'refund'},
-        {label:'Cancellation',value:'cancellation'}]"
+        :options="[
+          { label: 'Return', value: 'return' },
+          { label: 'Refund', value: 'refund' },
+          { label: 'Cancellation', value: 'cancellation' },
+        ]"
         v-model="issueDetails.issue"
         validation="required"
       >
@@ -24,27 +23,26 @@
         label="Status"
         placeholder="Status"
         :disabled="true"
-        :options="[{label:'active',value:'active'},
-      {label:'resolved',value:'resolved'}
-      ]"
-      
+        :options="[
+          { label: 'active', value: 'active' },
+          { label: 'resolved', value: 'resolved' },
+        ]"
         v-model="issueDetails.status"
       >
       </FormKit>
-    
-    <FormKit 
-    type="textarea" 
-    label="Concern" 
-    placeholder="Please explain your concern in detail..."
-    v-model="issueDetails.concern"
-    validation="required"
-     ></FormKit>
 
+      <FormKit
+        type="textarea"
+        label="Concern"
+        placeholder="Please explain your concern in detail..."
+        v-model="issueDetails.concern"
+        validation="required"
+      ></FormKit>
     </FormKit>
-    <br/>
-    <br/>
-    <br/>
-    <br/>
+    <br />
+    <br />
+    <br />
+    <br />
   </div>
 </template>
 
@@ -54,39 +52,37 @@ import { onMounted, ref, reactive } from "vue";
 import { useRoute } from "vue-router";
 import type { IGetIssueDetails } from "@/interfaces/report-issue";
 import auctionService from "@/services/auctionService";
-const details = localStorage.getItem("userDetails");
+const details: any = localStorage.getItem("userDetails");
 const { userId } = JSON.parse(details);
 
-
-
-let issueDetails =reactive<IGetIssueDetails>({
-  issue:"",
-  status:"",
-  concern:"",
+let issueDetails = reactive<IGetIssueDetails>({
+  issue: "",
+  status: "",
+  concern: "",
 });
-
 
 onMounted(async () => {
   await auctionService.getReport(userId);
-
 });
 
-const issueSubmit = () => {
-  auctionService.postReport(userId,issueDetails.issue,issueDetails.concern);
+const issueSubmit = async () => {
+  await auctionService.postReport(
+    userId,
+    issueDetails.issue,
+    issueDetails.concern,
+    ""
+  );
 
   //console.log(response);
-  issueDetails.status="active";
+  issueDetails.status = "active";
   //console.log(issueDetails.concern);
   //console.log("issue submitted");
   setTimeout(() => {
     window.location.reload();
   }, 2000);
-  console.log("reload")
+  console.log("reload");
 };
-
-
 </script>
-
 
 <style scoped>
 .side-bar > div {
@@ -103,4 +99,3 @@ const issueSubmit = () => {
   grid-template-columns: 20% 80%;
 }
 </style>
-
--- FrontEnd/src/services/auctionService.ts
+++ FrontEnd/src/services/auctionService.ts
@@ -15,13 +15,13 @@ export default {
     });
   },
 
-  getTopFiveUser(auction: Number | string){
-    return apiClient.post("api/auction/topFiveUsers",{
-      auctionId:auction
+  getTopFiveUser(auction: Number | string) {
+    return apiClient.post("api/auction/topFiveUsers", {
+      auctionId: auction,
     });
   },
 
-  getNewItemDetails(requestPayload:any){
+  getNewItemDetails(requestPayload: any) {
     return apiClient.post("api/auction/auctionItemDetails", requestPayload);
   },
 
@@ -44,30 +44,48 @@ export default {
     return apiClient.post("api/bid/placeBid", payload);
   },
 
-  getAllBidDetails(){
+  getAllBidDetails() {
     return apiClient.get("api/bid/fetchDetails");
   },
-  getWishlist(userId: Number){
-    return apiClient.get("api/wishlist/getWishlist?user_id="+userId);
+  getWishlist(userId: Number) {
+    return apiClient.get("api/wishlist/getWishlist?user_id=" + userId);
   },
-  postWishlist(item_id=Number, user_id=Number){
+  postWishlist(item_id: Number, user_id: Number) {
     console.log(item_id, user_id);
-    return apiClient.post("api/wishlist/addWishlist?item_id="+item_id+"&user_id="+user_id);
+    return apiClient.post(
+      "api/wishlist/addWishlist?item_id=" + item_id + "&user_id=" + user_id
+    );
   },
 
-  deleteWishlist(item_id=Number, user_id=Number){
-    return apiClient.delete("api/wishlist/deleteWishlist?item_id="+item_id+"&user_id="+user_id);
+  deleteWishlist(item_id: Number, user_id: Number) {
+    return apiClient.delete(
+      "api/wishlist/deleteWishlist?item_id=" + item_id + "&user_id=" + user_id
+    );
   },
-  
-  getReport(user_id=Number){
+
+  getReport(user_id: Number) {
     console.log(user_id);
-    return apiClient.get("api/report/getReport?user_id="+user_id);
+    return apiClient.get("api/report/getReport?user_id=" + user_id);
   },
-  postReport(user_id=Number, issue=String, concern=String,status=String){
-    return apiClient.post("api/report/addReport?user_id="+user_id+"&issueType="+issue+"&description="+concern+"&status="+status);
+  postReport(user_id: Number, issue: String, concern: String, status: String) {
+    return apiClient.post(
+      "api/report/addReport?user_id=" +
+        user_id +
+        "&issueType=" +
+        issue +
+        "&description=" +
+        concern +
+        "&status=" +
+        status
+    );
   },
-  updateReport(ticketId=Number, isResolved=Boolean){
+  updateReport(ticketId: Number, isResolved: Boolean) {
     console.log(ticketId, isResolved);
-    return apiClient.put("api/report/updateReport?ticketId="+ticketId+"&isResolved="+isResolved);
-  }
+    return apiClient.put(
+      "api/report/updateReport?ticketId=" +
+        ticketId +
+        "&isResolved=" +
+        isResolved
+    );
+  },
 };
