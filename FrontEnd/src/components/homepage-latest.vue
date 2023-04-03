7cbc0710a565c47b3848507243055eddaf72b540
Harsh Srivastava <hr765259@dal.ca>
2023-04-03 12:11:31 -0300

    Fix
    Fixed Design for filter icon on home page

--- FrontEnd/src/components/homepage-latest.vue
+++ FrontEnd/src/components/homepage-latest.vue
@@ -1,34 +1,40 @@
 <template>
   <loader v-if="isLoading"></loader>
   <template v-else>
-    <button @click="isHidden=!isHidden"><h5>Filter</h5></button>
-    <div v-show="isHidden">
-    <h6 style="display: flex; justify-content:center;">Date between</h6>
-    <div class="parent_sect">
-    <FormKit
-  type="date"
-  name="StartDate"
-  label="Start Date"
-  validation="required"
-  validation-visibility="live"
-  v-model="startDate"
-/>
-<FormKit
-  type="date"
-  label="End Date"
-  :validation="[['date_after', startDate]]"
-  v-model="endDate"
-/>
-
-</div>
-<div style="display: flex; justify-content: center;">
-<FormKit 
-type="button"
-label="Search"
-@click="filterByDate"
-/>
-</div>
-</div>
+    <div
+      class="filter-container"
+      style="display: flex; justify-content: flex-end"
+    >
+      <button class="filter-toggle" v-if="!isHidden" @click="showFilter">
+        <font-awesome-icon icon="filter" />
+      </button>
+      <div class="card filter-section" v-if="isHidden">
+        <div class="form-group w-40" style="display: inline-block">
+          <FormKit
+            type="date"
+            name="StartDate"
+            label="Start Date"
+            validation="required"
+            validation-visibility="live"
+            v-model="startDate"
+          />
+        </div>
+        <div class="form-group w-40" style="display: inline-block">
+          <FormKit
+            type="date"
+            label="End Date"
+            :validation="[['date_after', startDate]]"
+            v-model="endDate"
+          />
+        </div>
+        <div style="text-align: center">
+          <button class="btn btn-primary m-1" @click="filterByDate">
+            Search
+          </button>
+          <button class="btn btn-danger m-1" @click="close">Cancel</button>
+        </div>
+      </div>
+    </div>
     <Carousel
       id="gallery"
       :items-to-show="1"
@@ -40,7 +46,7 @@ label="Search"
           :src="item.imgUrl"
           onerror="this.src='https://imgs.search.brave.com/5W8zVnZVHamv7gy2RklV0IPv4-vJWrNe0wCqNTUjlDo/rs:fit:630:630:1/g:ce/aHR0cHM6Ly9yZXMu/Y2xvdWRpbmFyeS5j/b20vdGVlcHVibGlj/L2ltYWdlL3ByaXZh/dGUvcy0tNzlFd0pr/M3otLS90X1ByZXZp/ZXcvYl9yZ2I6MDAw/MDAwLGNfbGltaXQs/Zl9hdXRvLGhfNjMw/LHFfOTAsd182MzAv/djE2MDgyMzY0NDMv/cHJvZHVjdGlvbi9k/ZXNpZ25zLzE3NTE5/ODQ1XzAuanBn'"
           class="carousel__item item_size"
-          style="object-fit: contain;"
+          style="object-fit: contain"
           @click="liveAuctionClicked(item)"
         />
       </Slide>
@@ -62,7 +68,7 @@ label="Search"
       </Slide>
     </Carousel>
 
-    <h3 style="display: flex; justify-content:center;">Blind Bid</h3>
+    <h3 style="display: flex; justify-content: center">Blind Bid</h3>
     <Carousel>
       <Slide v-for="(item, index) in blindAuctionDetails" :key="index">
         <img
@@ -78,7 +84,7 @@ label="Search"
       </template>
     </Carousel>
 
-    <h3 style="display: flex; justify-content:center;">Simple sell</h3>
+    <h3 style="display: flex; justify-content: center">Simple sell</h3>
 
     <Carousel>
       <Slide v-for="(item, index) in simpleSellAuctionDetails" :key="index">
@@ -113,33 +119,45 @@ const slideTo = (val: any) => {
   currentSlide.value = val;
 };
 
+const close = () => {
+  isHidden.value = false;
+};
+
+const showFilter = () => {
+  isHidden.value = true;
+};
+
 const filterByDate = async (auctions: IGeneralAuctionDetails[]) => {
-    let response=await auctionService.getAllBidDetails();
+  let response = await auctionService.getAllBidDetails();
 
-    let allAuctionDetails = response.data.details.map((auction:any) => {
-      return {
-        auctionId: auction.auctionId,
-        auctionType: auction.auctionType,
-        startTime: auction.startTime,
-        endTime: auction.endTime,
-        ...auction.items[0],
-        imgUrl: auction.items[0].imageDetails[0].imgUrl,
-      };
-    });
-    blindAuctionDetails.value = allAuctionDetails.filter(
-      (auction:any) => getDate(auction.startTime) >= getDate(startDate.value) && getDate(auction.endTime) <= getDate(endDate.value) &&
-      auction.auctionType == "blind",
-    );
-    simpleSellAuctionDetails.value = allAuctionDetails.filter(
-      (auction:any) => auction.auctionType == null // update it to simple get it fixed with BE
-&& getDate(auction.startTime) >= getDate(startDate.value) && getDate(auction.endTime) <= getDate(endDate.value),
-
-      );
-    liveAuctionDetails.value = allAuctionDetails.filter(
-      (auction:any) => auction.auctionType == "live"
-&& getDate(auction.startTime) >= getDate(startDate.value) && getDate(auction.endTime) <= getDate(endDate.value),
-    );
-
+  let allAuctionDetails = response.data.details.map((auction: any) => {
+    return {
+      auctionId: auction.auctionId,
+      auctionType: auction.auctionType,
+      startTime: auction.startTime,
+      endTime: auction.endTime,
+      ...auction.items[0],
+      imgUrl: auction.items[0].imageDetails[0].imgUrl,
+    };
+  });
+  blindAuctionDetails.value = allAuctionDetails.filter(
+    (auction: any) =>
+      getDate(auction.startTime) >= getDate(startDate.value) &&
+      getDate(auction.endTime) <= getDate(endDate.value) &&
+      auction.auctionType == "blind"
+  );
+  simpleSellAuctionDetails.value = allAuctionDetails.filter(
+    (auction: any) =>
+      auction.auctionType == null && // update it to simple get it fixed with BE
+      getDate(auction.startTime) >= getDate(startDate.value) &&
+      getDate(auction.endTime) <= getDate(endDate.value)
+  );
+  liveAuctionDetails.value = allAuctionDetails.filter(
+    (auction: any) =>
+      auction.auctionType == "live" &&
+      getDate(auction.startTime) >= getDate(startDate.value) &&
+      getDate(auction.endTime) <= getDate(endDate.value)
+  );
 };
 const liveAuctionClicked = (args: any) => {
   let queryURL = {
@@ -176,11 +194,11 @@ let isHidden = ref<boolean>(false);
 
 onMounted(async () => {
   try {
-   // console.log(liveAuctionDetails);
+    // console.log(liveAuctionDetails);
     isLoading.value = true;
     const response = await auctionService.getAllBidDetails();
     //console.log(response.data.details);
-    let allAuctionDetails = response.data.details.map((auction:any) => {
+    let allAuctionDetails = response.data.details.map((auction: any) => {
       return {
         auctionId: auction.auctionId,
         auctionType: auction.auctionType,
@@ -190,16 +208,13 @@ onMounted(async () => {
     });
 
     blindAuctionDetails.value = allAuctionDetails.filter(
-      (auction:any) => auction.auctionType == "blind",
+      (auction: any) => auction.auctionType == "blind"
     );
     simpleSellAuctionDetails.value = allAuctionDetails.filter(
-      (auction:any) => auction.auctionType == null,// update it to simple get it fixed with BE
-
-
-      );
+      (auction: any) => auction.auctionType == null // update it to simple get it fixed with BE
+    );
     liveAuctionDetails.value = allAuctionDetails.filter(
-      (auction:any) => auction.auctionType == "live",
-
+      (auction: any) => auction.auctionType == "live"
     );
   } catch (e) {
     console.log("Error occured");
@@ -223,7 +238,7 @@ onMounted(async () => {
   justify-content: center;
   align-items: center;
 }
-.img_resolution{
+.img_resolution {
   width: 500px;
   height: 200px;
 }
@@ -240,7 +255,54 @@ onMounted(async () => {
 }
 
 .parent_sect {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-  }
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+}
+
+.filter-icon-container {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 1rem;
+}
+.filter-toggle {
+  background-color: #f5f5f5;
+  border: none;
+  padding: 0.5rem;
+  cursor: pointer;
+}
+.card.filter-section {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+  padding: 1rem;
+  margin: 1rem 0;
+  background-color: #f5f5f5;
+}
+.form-group {
+  display: flex;
+  flex-direction: column;
+  margin-right: 1rem;
+}
+.form-group label {
+  margin-bottom: 0.5rem;
+}
+.filter-btn {
+  background-color: #2b2d42;
+  color: #fff;
+  border: none;
+  padding: 0.5rem 1rem;
+  margin-top: 1rem;
+  cursor: pointer;
+}
+
+.filter-container {
+  display: flex;
+  align-items: center;
+}
+
+.filter-toggle {
+  margin-right: 1rem;
+}
 </style>
