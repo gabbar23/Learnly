7f6fbaca9f929d6b8b5194b04c3926b398f3c5e6
Kush Shaileshkumar Sutaria <ks428142@dal.ca>
2023-04-07 01:37:23 -0300

    fix: fixed redirect to home screen instead of login screen for signed in users

--- FrontEnd/src/router/index.ts
+++ FrontEnd/src/router/index.ts
@@ -145,11 +145,11 @@ router.beforeEach((to, from, next) => {
   const isBuyer= !!(userDetail && userDetail.isBuyer);
   const adminID= !! (userDetail && userDetail.email=="admin@bid4good.ca");
   if (isLoggedIn) {
-    console.log(userDetail)
+   //console.log(userDetail)
     store.commit("setSessionId", userDetail.sessionId);
   }
   store.commit("setCurrentRoute", to);
-  console.log(to);
+  //console.log(to);
   let buyerPages= (to.name==="buyer-order" || to.name==="buyer-report-issue" || to.name ==="buyer-issue-history");
   let adminPages= (to.name==="Admin Dashboard" || to.name==="Admin Issues");
 
@@ -160,6 +160,9 @@ router.beforeEach((to, from, next) => {
     next({ name: "Login" });
   }
   else if(isLoggedIn){
+    if(to.name=="Login"){
+      next({ name: "Home" });
+    }
     if (!adminID && adminPages) {
       next({ name: "No Access" });
     }
